0b2b6fc3ece87767e8ff88f5f7e500e0314e6af7
SeungTae <dltmdxo1589@daum.net>
2024-05-02 22:00:40 +0900

    validation class

---
+++ src/main/java/seungtae/product/management/service/ValidationService.java
@@ -0,0 +1,14 @@
+package seungtae.product.management.service;
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Service
+@Validated
+public class ValidationService {
+
+    public <T> void checkValid(@Valid T validationTarget) {
+        // do nothing
+    }
+}
