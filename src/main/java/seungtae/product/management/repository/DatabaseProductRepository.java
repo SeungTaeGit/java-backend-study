707f083b44deb9a4063106d7613ac687dbc09753
SeungTae <dltmdxo1589@daum.net>
2024-05-05 00:34:58 +0900

    Database linked repository

---
+++ src/main/java/seungtae/product/management/repository/DatabaseProductRepository.java
@@ -0,0 +1,58 @@
+package seungtae.product.management.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import seungtae.product.management.entity.Product;
+
+import java.util.Collections;
+import java.util.List;
+
+@Repository
+public class DatabaseProductRepository {
+
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Autowired
+    public DatabaseProductRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+    public Product add(Product product) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        SqlParameterSource namedParameter = new BeanPropertySqlParameterSource(product);
+
+        namedParameterJdbcTemplate
+                .update("INSERT INTO products (name, price, quantity) VALUES (:name, :price, :quantity)", namedParameter, keyHolder);
+
+        Long generatedId = keyHolder.getKey().longValue();
+        product.setId(generatedId);
+
+        return product;
+    }
+
+    public Product finById(Long id) {
+        return null;
+    }
+
+    public List<Product> findALl() {
+        return Collections.EMPTY_LIST;
+    }
+
+    public List<Product> findByNameContaining(String name) {
+        return Collections.EMPTY_LIST;
+    }
+
+    public Product update(Product product) {
+        return null;
+    }
+
+    public void delete(Long id) {
+        //do nothing
+    }
+}
